084ea3d6b86f92d8d39aafc883d9fb8a3d3f7117
zhangqw2 <z_q_wei@163.com>
2020-09-22 16:05:35 +0800

    更新单例模式

--- src/main/java/com/example/designpattern/singletonpattern/HungrySingleton.java
+++ src/main/java/com/example/designpattern/singletonpattern/HungrySingleton.java
@@ -7,6 +7,9 @@ package com.example.designpattern.singletonpattern;
 public class HungrySingleton {
     private final static HungrySingleton instance = new HungrySingleton();
 
+    /**
+     * 私有构造,限制产生多个对象
+     */
     private HungrySingleton() {
         //饿汉模式下，可以添加判断，阻止反射破坏单例
         if (instance != null) {
--- src/main/java/com/example/designpattern/singletonpattern/LazySingleton.java
+++ src/main/java/com/example/designpattern/singletonpattern/LazySingleton.java
@@ -7,12 +7,16 @@ package com.example.designpattern.singletonpattern;
 public class LazySingleton {
     private static LazySingleton lazySingleton = null;
 
+    /**
+     * 私有构造，限制产生多个对象
+     */
     private LazySingleton() {
 
     }
 
     public static LazySingleton getInstance() {
         if (lazySingleton == null) {
+            //高并发下,对象初始化需要时间，在内存中容易出现两个单例对象
             lazySingleton = new LazySingleton();
         }
         return lazySingleton;
